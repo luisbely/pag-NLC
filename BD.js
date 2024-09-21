560d4252fbbaf2b6b11c7efb7f1ea0f57fff646b
luisbely <166246008+luisbely@users.noreply.github.com>
2024-09-21 12:35:51 -0500

    Add files via upload

---
+++ BD.js
@@ -0,0 +1,16 @@
+let BD = [
+    {
+        nombre: "Bandana de Rap",
+        precio: 10000,
+        descripcion: "Bandana al estilo RAP de los 90's",
+        stock: 50,
+        imagen: "https://files.libertycity.net/download/gtasa_hats/thumbs/2020-02/rjeperskaja-bandana-dlja-sidzheja_1686003512_83395.jpg"
+    },
+    {
+        nombre: "Bandana de Rap",
+        precio: 10000,
+        descripcion: "Bandana al estilo RAP de los 90's",
+        stock: 50,
+        imagen: "https://files.libertycity.net/download/gtasa_hats/thumbs/2020-02/rjeperskaja-bandana-dlja-sidzheja_1686003512_83395.jpg"
+    },
+]
